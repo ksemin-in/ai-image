10c3f723ced14f45c96cf466fed841113fd0bda5
rohit <rohitgatne2300@gmail.com>
2026-01-23 10:24:02 +0530

    added json formating to output

--- img_desc.py
+++ img_desc.py
@@ -1,5 +1,22 @@
 import ollama
 import os
+from pydantic import BaseModel
+from typing import Optional, Literal
+
+
+class Object(BaseModel):
+  name: str
+  confidence: float
+  attributes: str
+
+class ImageDescription(BaseModel):
+  summary: str
+  objects: list[Object]
+  scene: str
+  colors: list[str]
+  time_of_day: Literal['Morning', 'Afternoon', 'Evening', 'Night']
+  setting: Literal['Indoor', 'Outdoor', 'Unknown']
+  text_content: Optional[str] = None
 
 def vision_query():
     # Initialize the client
@@ -21,6 +38,7 @@ def vision_query():
                 'content': user_prompt,
                 'images': [image_path] # Ensure this is a list of paths
             }],
+            format=ImageDescription.model_json_schema(),
             options={'num_gpu': 99}
         )
         
